aad2430ccb1d8dfaefd2fb48eb23da96bd6b610e
hemaksh3011 <hemakshkumawat1@gmail.com>
2025-06-27 19:14:51 +0530

    27-6

---
+++ sample.js
@@ -0,0 +1,38 @@
+  function test(){
+      var x=10;
+      if(true){
+          var x = 20;
+          console.log(x);
+      }
+      console.log(x);
+  }
+
+  test();
+
+
+ immediately involve function
+
+  (
+      function(){
+          console.log("hello");
+      }
+  )()
+
+
+  template littrals
+
+  const hello=(fruit)=>{
+      console.log(`my favrouit ffuit is ${fruit}`);
+
+  }
+  hello("apple");\
+
+
+
+  let classname = ["aa","bb","cc","dd"];
+  for(i=0;i<classname.length;i++){
+      console.log(`my class is ${classname}`);
+
+     } 
+
+
