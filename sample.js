ca8777e908a06407af54b2e47d5c26d5bda18567
hemaksh3011 <hemakshkumawat1@gmail.com>
2025-06-30 09:10:30 +0530

    js

--- sample.js
+++ sample.js
@@ -1,38 +1,38 @@
-  function test(){
-      var x=10;
-      if(true){
-          var x = 20;
-          console.log(x);
-      }
-      console.log(x);
-  }
+//   function test(){
+//       var x=10;
+//       if(true){
+//           var x = 20;
+//           console.log(x);
+//       }
+//       console.log(x);
+//   }
 
-  test();
-
-
- immediately involve function
-
-  (
-      function(){
-          console.log("hello");
-      }
-  )()
+//   test();
 
 
-  template littrals
+//  immediately involve function
 
-  const hello=(fruit)=>{
-      console.log(`my favrouit ffuit is ${fruit}`);
+//   (
+//       function(){
+//           console.log("hello");
+//       }
+//   )()
 
-  }
-  hello("apple");\
+
+//   template littrals
+
+//   const hello=(fruit)=>{
+//       console.log(`my favrouit ffuit is ${fruit}`);
+
+//   }
+//   hello("apple");\
 
 
 
-  let classname = ["aa","bb","cc","dd"];
-  for(i=0;i<classname.length;i++){
-      console.log(`my class is ${classname}`);
+//   let classname = ["aa","bb","cc","dd"];
+//   for(i=0;i<classname.length;i++){
+//       console.log(`my class is ${classname}`);
 
-     } 
+//      } 
 
 
